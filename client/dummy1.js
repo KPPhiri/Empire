4a8d78b01c3a9a8056725e32ef2a72bd2f96cbe5
kpphiri <kphiri6@gatech.edu>
2018-07-31 13:59:29 -0400

    Updating dummy.js

--- client/dummy1.js
+++ client/dummy1.js
@@ -1,2 +1,14 @@
 var rec = require('./dummy2.js');
 console.log(rec);
+
+//testing for now
+
+var rec = require('./dummy2.js');
+console.log(rec);
+
+//testing for now
+
+var rec = require('./dummy2.js');
+console.log(rec);
+
+//testing for now
